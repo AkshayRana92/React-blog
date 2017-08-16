305865eef172230cbace7fa9f2c8bd1bbf5c4a73
AkshayRana92 <akshayrana@knoldus.in>
2017-08-16 14:24:34 +0530

    Updated for first blog

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
-import Stopwatch from './Stopwatch';
-import Child from './Child';
 import MyComponent from './MyComponent';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  constructor() {
-    super()
-    this.state = {name: 'Say my name'}
-  }
-
-  handleName(newName) {
-    this.setState({ name: newName});
-  }
 
   render() {
     return (
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,20 +2,9 @@ import React, { Component } from 'react';
 
 class MyComponent extends Component {
 
-    constructor() {
-        super();
-        this.state= {name: 'Say my name'};
-        setInterval(
-            () => {
-                this.setState({name: 'Heisenberg'})
-            },
-            2000
-        );
-    }
-
     render() {
         return (
-            <h1>{this.state.name}</h1>
+            <h1>Heisenberg</h1>
         );
     }
 }
